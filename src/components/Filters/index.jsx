71cde1bd2b838fac98f620feb15013a3db7ba3c6
rmarinriv <rmarinriv@gmail.com>
2020-07-18 17:11:45 +0200

    Search suggestions

--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
 import { Search } from 'react-bootstrap-icons';
 
 // Actions
@@ -19,8 +21,40 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 function Filters(props) {
-  const { state } = props;
-  const { order } = state;
+  const { state, setSearch: setNewSearch } = props;
+  const { order, search, content } = state;
+  let searchInput = null;
+  // Apply search
+  const searchedContent = search === '' ? content
+    : _.filter(content, (element) => (
+      element.title.toLowerCase().includes(search.toLowerCase())
+      || element.summary.toLowerCase().includes(search.toLowerCase())
+    ));
+
+  const handleSuggestion = (e) => {
+    const { type, keyCode, currentTarget } = e;
+
+    if (type === 'click' || keyCode === 13) {
+      const { innerText } = currentTarget;
+
+      searchInput.value = innerText;
+      setNewSearch(innerText);
+    }
+  };
+
+  const listItems = searchedContent.map((element) => (
+    <ListGroup.Item
+      key={element.id}
+      tabIndex={0}
+      style={{
+        padding: '.375rem .75rem',
+        cursor: 'pointer',
+      }}
+      onClick={(e) => handleSuggestion(e)}
+    >
+      {element.title}
+    </ListGroup.Item>
+  ));
 
   // Alphabetically sort using title
   const handleSort = (e) => {
@@ -42,7 +76,6 @@ function Filters(props) {
 
   // Search using title or summary
   const handleSearch = (e) => {
-    const { setSearch: setNewSearch } = props;
     const { currentTarget } = e;
     const { value } = currentTarget;
 
@@ -57,36 +90,55 @@ function Filters(props) {
           alignItems: 'center',
         }}
       >
-        <span
-          role="button"
-          tabIndex={0}
+        <Button
+          variant="primary"
           onClick={(e) => handleSort(e)}
           onKeyDown={(e) => handleSort(e)}
         >
           Sort
           {' '}
-          {order === 'asc' ? '▼' : '▲'}
-        </span>
+          {order === 'asc' ? '▲' : '▼'}
+        </Button>
       </Col>
       <Col xs={12} sm={6}>
-        <Form inline className="float-sm-right">
-          <Button
-            variant="outline-success"
+        <Form
+          inline
+          className="float-sm-right"
+          style={{
+            position: 'relative',
+          }}
+        >
+          <span
             style={{
               position: 'absolute',
+              padding: '.375rem .75rem',
               backgroundColor: 'transparent',
               borderColor: 'transparent',
               pointerEvents: 'none',
             }}
           >
             <Search color="black" />
-          </Button>
+          </span>
           <FormControl
             type="text"
             placeholder="Search"
             className="pl-5"
+            ref={(input) => { searchInput = input; }}
             onKeyUp={(e) => handleSearch(e)}
           />
+          {search !== ''
+            && (
+            <ListGroup
+              style={{
+                position: 'absolute',
+                top: '100%',
+                width: '100%',
+                zIndex: 1,
+              }}
+            >
+              {listItems}
+            </ListGroup>
+            )}
         </Form>
       </Col>
     </Row>
@@ -96,6 +148,7 @@ function Filters(props) {
 Filters.propTypes = {
   state: PropTypes.shape({
     order: PropTypes.string.isRequired,
+    search: PropTypes.string.isRequired,
     content: PropTypes.arrayOf(PropTypes.object).isRequired,
   }).isRequired,
   setOrder: PropTypes.func.isRequired,
