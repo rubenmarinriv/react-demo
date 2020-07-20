7c3e6fbbf5919143f7a1777f5176e2add63076d1
rmarinriv <rmarinriv@gmail.com>
2020-07-20 12:44:22 +0200

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import {
-  Navbar, Nav, Container,
-} from 'react-bootstrap';
-import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
 } from 'react-router-dom';
+import {
+  Navbar, Nav, Container,
+} from 'react-bootstrap';
 
 // Redux actions
 import { setContent } from './actions';
@@ -18,7 +18,8 @@ import { setContent } from './actions';
  * Movie and series data
  *
  * If we had a backend we would make a request to the server
- * to get the data using fetch or some module like axios
+ * using fetch or axios and we would handle possible errors
+ * using an Alert component
  */
 import content from './data/content.json';
 
@@ -27,22 +28,28 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Logout from './components/Logout';
 
+// Redux dispatchers
 const mapDispatchToProps = (dispatch) => ({
   setContent: (newContent) => dispatch(setContent(newContent)),
 });
 
-function App(props) {
+const App = (props) => {
+  // Handle "auth" in App using useState Hook, false by default
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  /*
+   * Set "fetched" movie and series data in Redux store once
+   * App is mounted
+   */
   useEffect(() => {
     const { setContent: setNewContent } = props;
 
-    // Set "fetched" movie and series data in Redux store
     setTimeout(() => {
       setNewContent(content);
-    }, 100);
+    }, 100); // Simulate delay
   }, []);
 
+  // Simulate auth
   const fakeAuth = {
     login(cb) {
       setIsAuthenticated(true);
@@ -57,52 +64,20 @@ function App(props) {
   return (
     <div className="App">
       <Router>
-        <Navbar
-          className="mb-3"
-          collapseOnSelect
-          expand="md"
-          bg="dark"
-          variant="dark"
-          style={{
-            zIndex: 2,
-          }}
-        >
+        <Navbar className="mb-3" collapseOnSelect expand="md" bg="dark" variant="dark" style={{ zIndex: 2 }}>
           <Container>
             <Navbar.Brand>
-              <Link
-                to="/"
-                style={{
-                  color: 'white',
-                }}
-              >
-                ACME
-              </Link>
+              <Link to="/" style={{ color: 'white' }}>ACME</Link>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto" />
               <Nav>
                 { !isAuthenticated && (
-                  <Link
-                    className="nav-link"
-                    to="/login"
-                    style={{
-                      color: 'white',
-                    }}
-                  >
-                    Login
-                  </Link>
+                  <Link className="nav-link" to="/login" style={{ color: 'white' }}>Login</Link>
                 )}
                 { isAuthenticated && (
-                  <Link
-                    className="nav-link"
-                    to="/logout"
-                    style={{
-                      color: 'white',
-                    }}
-                  >
-                    Logout
-                  </Link>
+                  <Link className="nav-link" to="/logout" style={{ color: 'white' }}>Logout</Link>
                 )}
               </Nav>
             </Navbar.Collapse>
@@ -122,10 +97,12 @@ function App(props) {
       </Router>
     </div>
   );
-}
+};
 
+// Validate data types
 App.propTypes = {
   setContent: PropTypes.func.isRequired,
 };
 
+// Connect dispatchers to App props
 export default connect(null, mapDispatchToProps)(App);
