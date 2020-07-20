75ce00b7cd0feb6e6673f8f755228f6d5014e4df
rmarinriv <rmarinriv@gmail.com>
2020-07-20 13:19:26 +0200

    Refactor

--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -11,12 +11,13 @@ import Sort from './Sort';
 import Dropdown from './Dropdown';
 import SearchBar from './SearchBar';
 
+// Redux dispatchers
 const mapDispatchToProps = (dispatch) => ({
   setGenre: (newGenre) => dispatch(setGenre(newGenre)),
   setType: (newType) => dispatch(setType(newType)),
 });
 
-function Filters(props) {
+const Filters = (props) => {
   const { setGenre: setNewGenre, setType: setNewType } = props;
 
   return (
@@ -25,29 +26,23 @@ function Filters(props) {
         <Sort />
       </Col>
       <Col xs={4} md={2}>
-        <Dropdown
-          defaultText="Genre"
-          optionsKey="genres"
-          set={setNewGenre}
-        />
+        <Dropdown defaultText="Genre" optionsKey="genres" set={setNewGenre} />
       </Col>
       <Col xs={4} md={2}>
-        <Dropdown
-          defaultText="Type"
-          optionsKey="type"
-          set={setNewType}
-        />
+        <Dropdown defaultText="Type" optionsKey="type" set={setNewType} />
       </Col>
       <Col xs={12} md={6}>
         <SearchBar />
       </Col>
     </Row>
   );
-}
+};
 
+// Validate data types
 Filters.propTypes = {
   setGenre: PropTypes.func.isRequired,
   setType: PropTypes.func.isRequired,
 };
 
+// Connect dispatchers to Filters props
 export default connect(null, mapDispatchToProps)(Filters);
